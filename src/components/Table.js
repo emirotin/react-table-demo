0467dd412bea3b5db01efc1171037d2f12cc6291
Eugene Mirotin <emirotin@gmail.com>
2016-09-09 14:31:22 +0300

    add sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react'
+import { orderBy } from 'lodash'
 
 import TableHead from './TableHead'
 import TableBody from './TableBody'
 
 export default class Table extends Component {
-  constructor(args) {
-    super(...args)
+  constructor(props, args) {
+    super(props, ...args)
+
+    const columns = this.extractColumns(props.data)
     this.state = {
-      sorting: {
-        column: null,
-        direction: null
-      }
+      columns,
+      sorting: this.getDefaultSorting(columns)
     }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { data } = nextProps
+    const columns = this.extractColumns(data)
+    const nextState = { columns }
+    if (!columns.includes(this.state.column)) {
+      nextState.sorting = this.getDefaultSorting(columns)
+    }
+    this.setState(nextState)
   }
 
   extractColumns(data) {
     return data && data.length ? Object.keys(data[0]) : []
   }
 
-  render() {
+  getDefaultSorting(columns) {
+    return {
+      column: columns[0],
+      direction: true
+    }
+  }
+
+  getSortedData() {
     const { data } = this.props
-    const columns = this.extractColumns(data)
+    const { sorting } = this.state
+    return orderBy(data, [ sorting.column ], [ sorting.direction ? 'asc' : 'desc' ])
+  }
+
+  setSorting = (column) => {
+    const { sorting: { column: currentColumn, direction: currentDirection } } = this.state
+    const direction = column === currentColumn
+      ? !currentDirection
+      : true
+    this.setState({
+      sorting: { column, direction }
+    })
+  }
+
+  render() {
+    const data = this.getSortedData()
+    const { columns, sorting } = this.state
     return <table className="table">
-      <TableHead columns={columns} />
+      <TableHead columns={columns} sorting={sorting} setSorting={this.setSorting} />
       <TableBody data={data} columns={columns} />
     </table>
   }
--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
 export default class TableHead extends Component {
-  renderColumn = (column) => {
-    return <th key={column}>{column}</th>
+  setSorting = (column) => {
+    this.props.setSorting(column)
   }
+
+  renderChevron(direction) {
+    return <i className={
+      classnames('glyphicon', {
+        'glyphicon-chevron-down': !direction,
+        'glyphicon-chevron-up': direction
+      })
+    }></i>
+  }
+
+  renderColumn = (column) => {
+    const { sorting } = this.props
+
+    return <th key={column}>
+      <button className="btn btn-link btn-md"
+        onClick={() => this.setSorting(column)}>{column}</button>
+      {sorting.column === column &&
+        this.renderChevron(sorting.direction)
+      }
+    </th>
+  }
+
   render() {
     return <thead>
       <tr>{this.props.columns.map(this.renderColumn)}</tr>
